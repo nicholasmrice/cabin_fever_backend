9dffcfe8eff8b3c51c48101ba4d1a8d1572a7408
Your Name <you@example.com>
2021-01-29 00:57:26 -0700

    basic crud

--- app/models/cabin.rb
+++ app/models/cabin.rb
@@ -6,46 +6,45 @@ class Cabin
       return results.each do |result|
         {
           "id" => result["id"].to_i,
-          "lattitude" => result["lattitude"],
-          "longitude" => result["longitude"],
+          # "lattitude" => result["lattitude"],
+          # "longitude" => result["longitude"],
           "address" => result["address"],
-          "image" => result["image"],
+          # "image" => result["image"],
           "description" => result["description"],
-          "amenities" => result["amenities"],
+          # "amenities" => result["amenities"],
         }
       end
-    end
-
-  def self.find(id)
-    results = DB.exec("SELECT * FROM cabins WHERE id=#{id};")
-    return {
-         "id" => result["id"].to_i,
-         "lattitude" => result["lattitude"],
-         "longitude" => result["longitude"],
-         "address" => result["address"],
-         "image" => result["image"],
-         "description" => result["description"],
-         "amenities" => result["amenities"],
-    }
   end
+
+  # def self.find(id)
+  #   results = DB.exec("SELECT * FROM cabins WHERE id=#{id};")
+  #   return {
+  #        "id" => result["id"].to_i,
+  #        # "lattitude" => result["lattitude"],
+  #        # "longitude" => result["longitude"],
+  #        "address" => result["address"],
+  #        # "image" => result["image"],
+  #        "description" => result["description"],
+  #        # "amenities" => result["amenities"],
+  #   }
+  # end
 
   def self.create(opts)
     results = DB.exec(
         <<-SQL
-            INSERT INTO cabins (lattitude, longitude, address, image, description, amenities)
-            VALUES (
-              #{opts["lattitude"]}, #{opts["longitude"]}, #{opts["address"]}, #{opts["image"]}, #{opts["description"]}, #{opts["amenities"]},  )
-            RETURNING id, lattitude, longitude, address, image, description, amenities;
+            INSERT INTO cabins (address, description)
+            VALUES ('#{opts["address"]}', '#{opts["description"]}')
+            RETURNING id, address, description;
         SQL
     )
     return {
          "id" => results.first["id"].to_i,
-         "lattitude" => results.first["lattitude"],
-         "longitude" => results.first["longitude"],
+         # "lattitude" => results.first["lattitude"],
+         # "longitude" => results.first["longitude"],
          "address" => results.first["address"],
-         "image" => results.first["image"],
+         # "image" => results.first["image"],
          "description" => results.first["description"],
-         "amenities" => results.first["amenities"],
+         # "amenities" => results.first["amenities"],
     }
   end
 
@@ -58,19 +57,19 @@ class Cabin
     results = DB.exec(
         <<-SQL
             UPDATE cabins
-              SET lattitude=#{opts["lattitude"]}, longitude=#{opts["longitude"]}, address=#{opts["address"]}, image=#{opts["image"]}, description=#{opts["description"]} amenities=#{opts["amenities"]},  
+              SET address='#{opts["address"]}', description='#{opts["description"]}'
               WHERE id=#{id}
-            RETURNING id, lattitude, longitude, address, image, description, amenities;
+            RETURNING id, address, description;
         SQL
     )
     return {
          "id" => results.first["id"].to_i,
-         "lattitude" => results.first["lattitude"],
-         "longitude" =>  results.first["longitude"],
+         # "lattitude" => results.first["lattitude"],
+         # "longitude" =>  results.first["longitude"],
          "address" => results.first["address"],
-         "image" => results.first["image"],
+         # "image" => results.first["image"],
          "description" => results.first["description"],
-         "amenities" => results.first["amenities"],
+         # "amenities" => results.first["amenities"],
     }
   end
 end
